1ec44e699c427a33a756ffaf39183a137dada395
howardhung0314 <howard@ztore.com>
2022-09-14 16:03:49 +0800

    gitsearch temp

--- 源码/react_staging/howard-test/src/App.jsx
+++ 源码/react_staging/howard-test/src/App.jsx
@@ -1,35 +1,13 @@
-//创建“外壳”组件App
 import React, { Component } from "react";
-import axios from "axios";
+import Search from "./components/GitSearch/Search";
+import List from "./components/GitSearch/List";
 
 export default class App extends Component {
-  getStudentData = () => {
-    axios.get("http://localhost:3000/api1/students").then(
-      (response) => {
-        console.log("success", response.data);
-      },
-      (error) => {
-        console.log("error", error);
-      }
-    );
-  };
-
-  getCarData = () => {
-    axios.get("http://localhost:3000/api2/cars").then(
-      (response) => {
-        console.log("success", response.data);
-      },
-      (error) => {
-        console.log("error", error);
-      }
-    );
-  };
-
   render() {
     return (
-      <div>
-        <button onClick={this.getStudentData}>點我取得學生資料</button>
-        <button onClick={this.getCarData}>點我取得汽車資料</button>
+      <div className="container">
+        <Search />
+        <List />
       </div>
     );
   }
