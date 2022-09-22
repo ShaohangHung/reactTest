af3898cb0c6027edb27c7acb8aa81966f5daeb18
howardhung0314 <howard@ztore.com>
2022-09-22 14:19:37 +0800

    antd lib practice

--- 源码/react_staging/howard-test/src/App.jsx
+++ 源码/react_staging/howard-test/src/App.jsx
@@ -1,49 +1,24 @@
-import React, { Component } from "react";
-import { Routes, Route, HashRouter, Navigate } from "react-router-dom";
-import About from "./components/Router/About";
-import Home from "./components/Router/Home";
-import Header from "./components/Router/Header";
-import MyNavLink from "./components/MyNavLink";
+import React from "react";
+import { DatePicker, Button } from "antd";
+import { AppleOutlined, SearchOutlined } from "@ant-design/icons";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        {/*HashRouter要包含註冊NavLink和註冊Route*/}
-        <HashRouter>
-          <div className="row">
-            <div className="col-xs-offset-2 col-xs-8">
-              <div className="page-header">
-                <Header a={1} />
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xs-2 col-xs-offset-2">
-              <div className="list-group">
-                {/*註冊NavLink*/}
-                <MyNavLink to="/about">About</MyNavLink>
-                <MyNavLink to="/home">Home</MyNavLink>
-              </div>
-            </div>
-            <div className="col-xs-6">
-              <div className="panel">
-                <div className="panel-body">
-                  {/*註冊Route*/}
-                  <Routes>
-                    <Route path="/about" element={<About a={1} />} />
-                    <Route path="/home/*" element={<Home a={1} />} />
-                    <Route
-                      path="/"
-                      element={<Navigate replace to="/about" />}
-                    />
-                  </Routes>
-                </div>
-              </div>
-            </div>
-          </div>
-        </HashRouter>
-      </div>
-    );
-  }
+function App() {
+  const onChange = (d,ds) => {
+    console.log(d);
+    console.log(ds);
+  };
+
+  return (
+    <div>
+      App
+      <button type="primary">Click Me</button>
+      <Button type="primary">Click Me</Button>
+      <Button type="link">Link Button</Button>
+      <AppleOutlined />
+      <Button icon={<SearchOutlined />}>Search</Button>
+      <DatePicker onChange={onChange} />
+    </div>
+  );
 }
+
+export default App;
